1b044b8d8825c643030b237be26e96433ed05fc0
HKalpesh <kalpesh08011@gmail.com>
2026-05-28 21:12:43 +0530

    -

--- basics/array_traversing.cpp
+++ basics/array_traversing.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     vector<int> a={1,2,3,4};
     for(int i=0;i<4;i++){
-        cout<<i<< =<<a[i]<< endl;
+        cout<<i<<" "<<a[i]<< endl;
     }
     return 0;
 }
