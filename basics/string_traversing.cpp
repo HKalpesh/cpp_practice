0dc420aae38148c5758250dddc2d3969c921d925
HKalpesh <kalpesh08011@gmail.com>
2026-06-01 16:49:57 +0530

    string traversing from 0 to -1 index

---
+++ basics/string_traversing.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string sample_string ="hello world !";
+
+    for (int index =0; index<sample_string.length();index++){
+        char ch= sample_string[index];
+        cout<<"index: "<< index<<" character: "<<ch<<endl;
+
+    }
+    return 0;
+}
