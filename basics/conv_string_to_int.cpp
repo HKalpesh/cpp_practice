6155a2ab682d6191df1d867637319b6478f88097
HKalpesh <kalpesh08011@gmail.com>
2026-06-02 17:01:01 +0530

    feat(basics): implement string to integer conversion
    Add a new implementation for converting strings to integers within the basics directory.

---
+++ basics/conv_string_to_int.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string numstring="123";
+
+    int parnum=stoi(numstring);
+
+    cout<<"original string"<<" "<< numstring<< endl;
+    cout<<"parsed string"<<" "<< parnum<< endl;
+    return 0;
+}
