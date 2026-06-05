64671f2c92354c699f2c0ec370a30067ec6e0272
HKalpesh <kalpesh08011@gmail.com>
2026-06-05 23:45:34 +0530

    feat(codeforce): add three_btothers solution for 800-1000 difficulty level

---
+++ codeforce/800-1000/three_btothers.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b ;
+    int total_brother=6;
+    cin>>a>>b;
+    int late = total_brother-(a+b);
+    cout<<late<<endl;
+}
