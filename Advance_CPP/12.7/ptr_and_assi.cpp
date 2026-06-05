1141d879af388f8dd46426a8465e8b30b1990192
HKalpesh <kalpesh08011@gmail.com>
2026-06-05 23:44:42 +0530

    feat(advance-cpp): add pointer and assignment example code

---
+++ Advance_CPP/12.7/ptr_and_assi.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x{5};
+    int* ptr{&x};// pointer initialized to point at x ;
+    cout<<*ptr<<endl;// prints the value at the add being pointer to (x's address )
+    int y 
+
+}
